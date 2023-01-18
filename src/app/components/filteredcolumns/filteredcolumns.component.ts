07f6d90167fece392f43199dcd27b1d6395a7db6
D1maD1m0nd <d1m0hkrasav4ik@gmail.com>
2023-01-19 00:03:43 +0300

    add filteredcolumns.component.ts components

---
+++ src/app/components/filteredcolumns/filteredcolumns.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filteredcolumns',
+  templateUrl: './filteredcolumns.component.html',
+  styleUrls: ['./filteredcolumns.component.css']
+})
+export class FilteredcolumnsComponent {
+
+}
