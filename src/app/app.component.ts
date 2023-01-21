74f1402b8525e9f6a55ce82cbc7708b9b5c8db73
D1maD1m0nd <d1m0hkrasav4ik@gmail.com>
2023-01-22 00:07:57 +0300

    add error service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CostItems} from "./components/data/mock/ItemCosts";
 import {CostItemsColumns} from "./components/data/mock/ItemColumns";
+import {ApiService} from "./services/apiservice.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  itemCosts = CostItems
-  itemColumns = CostItemsColumns
+export class AppComponent implements OnInit {
+    itemCosts = CostItems
+    itemColumns = CostItemsColumns
+
+    constructor(public apiService: ApiService) {
+    }
+
+    ngOnInit(): void {
+        this.apiService.GetVisaSummary().subscribe((i) => {
+            console.log(i);
+        })
+    }
 }
--- src/app/services/error.service.ts
+++ src/app/services/error.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Subject} from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ErrorService {
+    error$ = new Subject<string>()
 
-  constructor() { }
+    handler(message: string) {
+        this.error$.next(message)
+    }
+
+    clear() {
+        this.error$.next('')
+    }
 }
