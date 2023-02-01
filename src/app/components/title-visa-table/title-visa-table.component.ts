b6fc3cff02dbfba23267f3fa42ecfb32f2839ada
D1maD1m0nd <d1m0hkrasav4ik@gmail.com>
2023-02-01 13:42:46 +0300

    add title visa table component

---
+++ src/app/components/title-visa-table/title-visa-table.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-title-visa-table',
+    templateUrl: './title-visa-table.component.html',
+    styleUrls: ['./title-visa-table.component.css']
+})
+export class TitleVisaTableComponent {
+
+}
