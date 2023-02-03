3b1261cb54d1a2820c083212b17fe1ae5d83ca01
D1maD1m0nd <d1m0hkrasav4ik@gmail.com>
2023-02-03 09:25:19 +0300

    add converter service

---
+++ src/app/services/converter.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {Subject} from "rxjs";
+import {IVisaCostSummary} from "../components/data/model/response/VisaCostSummary";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ConverterService {
+    VisaCostSummary$ = new Subject<IVisaCostSummary>()
+
+    convert(json: string) {
+        const obj = JSON.parse(json);
+        console.log(obj)
+        this.VisaCostSummary$.next(obj)
+    }
+
+    constructor() {
+    }
+}
